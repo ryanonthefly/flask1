3bf18fcfa6d72b696308f3dc8b695901be5b5d6d
Huan Guo <huanguo@meta.com>
2025-08-03 08:20:01 -0700

    Update port for the easy testing

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def test_page():
     return render_template("test.html", message=message)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=80)
